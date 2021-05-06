31bcb13e7a187fa91a24aa936c3447567aed7361
Will Fantom <willfantom@gmail.com>
2021-05-06 21:14:17 +0100

    Provide log level prompt if too low
    Signed-off-by: Will Fantom <willfantom@gmail.com>

--- overseerr-exporter.go
+++ overseerr-exporter.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		if logrus.GetLevel() < logrus.WarnLevel {
-
+			logrus.Errorln("debug error by incraseing log level (e.g. debug)")
 		}
 		logrus.WithField("extended", err.Error()).
 			Fatalln("an error occurred executing the command")
